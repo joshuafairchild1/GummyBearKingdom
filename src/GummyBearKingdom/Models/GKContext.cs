21db760eaf187ef879fd58baaa64cb038d7a3cc0
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-04 09:35:15 -0700

    add initial database migration

---
+++ src/GummyBearKingdom/Models/GKContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GummyBearKingdom.Models
+{
+    public class GKContext : DbContext
+    {
+        public GKContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummyBearKingdom;integrated security=True");
+        }
+
+        public GKContext(DbContextOptions<GKContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/GummyBearKingdom/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummyBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string OriginCountry { get; set; }
+    }
+}
