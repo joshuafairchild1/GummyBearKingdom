0b69603248f3047258156cf234699b6740e072c5
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-04 10:33:44 -0700

    add full CRUD fn'ality for Product class

--- src/GummyBearKingdom/Controllers/ProductsController.cs
+++ src/GummyBearKingdom/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using GummyBearKingdom.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace GummyBearKingdom.Controllers
 {
@@ -10,6 +11,54 @@ namespace GummyBearKingdom.Controllers
         public IActionResult Index()
         {
             return View(db.Products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Product product = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Product product = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
