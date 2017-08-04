857e5fba72dcebd25885cc020bcbd51cc60a6527
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-04 09:45:58 -0700

    add products controller + index view

---
+++ src/GummyBearKingdom/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using GummyBearKingdom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GummyBearKingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GKContext db = new GKContext();
+        public IActionResult Index()
+        {
+            return View(db.Products);
+        }
+    }
+}
